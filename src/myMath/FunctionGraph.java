9d03d5e93de34b95eac5402a21d239c4e0026615
adiweisberg <adi.weisberg5@gmail.com>
2018-11-15 18:05:48 +0200

    update: Junit for the Polynom class

--- src/myMath/FunctionGraph.java
+++ src/myMath/FunctionGraph.java
@@ -19,39 +19,28 @@ public class FunctionGraph extends JFrame {
 		DataTable critical = new DataTable(Double.class, Double.class);
 		Polynom_able p2 = p1.copy();
 		p2 = p2.derivative();
-		int counter=0;
-		double x3 = 0;
-		double x2 = 0;
+		double x3=0,x2=0;
 		for (double x = -2.0; x <= 6.0; x+=0.25) {
 			double y = p1.f(x);
 			data.add(x, y);
 			x2 = x+0.25;
 			if(IsCritical(p2,x)==0) {
-				System.out.println("X = "+x+" , Y = "+y);
 				critical.add(x, y);
-				counter++;
 			}
 			else if(IsCritical(p2,x)!=IsCritical(p2,x2) && IsCritical(p2,x2) !=0) {
 				x3 = p2.root(x,x2,0.0001);
 				critical.add(x3,p1.f(x3));
-				counter++;
-				System.out.println("X = "+x+" , Y = "+y);
 			}
-
 		}
-		System.out.println(counter);
 		XYPlot plot = new XYPlot(data,critical);
 		getContentPane().add(new InteractivePanel(plot));
 		LineRenderer lines = new DefaultLineRenderer2D();
 		plot.setLineRenderers(data, lines);
 		Color color = new Color(0.0f, 0.0f, 0.0f);
-//		Color color2 = new Color(0.6f, 0.0f, 0.0f);
 		plot.getPointRenderers(data).get(0).setColor(color);
 		plot.getLineRenderers(data).get(0).setColor(color);
 		if(!plot.getPointRenderers(critical).isEmpty()) plot.getPointRenderers(critical).get(0).setColor(color.pink);
-		
 	}
-
 	public int IsCritical(Polynom_able deriv,double x) {
 		int flg=1;
 		if(Math.abs(deriv.f(x))<0.25) flg = 0;
@@ -61,9 +50,10 @@ public class FunctionGraph extends JFrame {
 
 
 	public static void main(String[] args) {
-		Polynom p1 = new Polynom("0.2x^4-1.5x^3+3.0x^2-x-5");
-		FunctionGraph frame = new FunctionGraph(p1);
-		frame.setVisible(true);
+		//Polynom_able p1 = new Polynom("2x^3 + -2.2X - 6");
+	    //FunctionGraph frame = new FunctionGraph(p1);
+		//frame.setVisible(true);
+		Polynom p1 = new Polynom("+5x^2 + -3x");
+		System.out.println(p1);
 	}
-
 }
